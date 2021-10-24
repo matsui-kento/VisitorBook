e3fd6a31581feec8ffabcbf7a8f0611582e9e32c
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-10-24 12:58:56 +0900

    Create CreateDescriptionViewController

---
+++ VisitorBook/Controllers/CreateDescriptionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateDescriptionViewController.swift
+//  VisitorBook
+//
+//  Created by matsui kento on 2021/10/24.
+//
+
+import UIKit
+
+class CreateDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func toCreateBookFormVC(_ sender: Any) {
+    }
+    
+    
+}
