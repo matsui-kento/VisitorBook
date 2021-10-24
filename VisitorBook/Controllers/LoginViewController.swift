5d390370ebdd3f8944d509f2dac4cd8bf4bfe621
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-10-24 15:24:59 +0900

    Create LoginViewController

---
+++ VisitorBook/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  VisitorBook
+//
+//  Created by matsui kento on 2021/10/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
