00d1a0c861eecc49ee1489e3347d7ceafcf6ae59
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-10-24 21:16:25 +0900

    ログイン機能と新規登録機能の追加

--- VisitorBook/Controllers/LoginViewController.swift
+++ VisitorBook/Controllers/LoginViewController.swift
@@ -6,13 +6,53 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    var delegate: BackSettingVCProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        Auth.loginUserWithAuth(email: email, password: password) { success in
+            if success {
+                self.delegate?.updateButton(controller: self)
+            }
+        }
+    }
+    
+    @IBAction func toSettingVC(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func toRegisterVC(_ sender: Any) {
+        self.delegate?.toRegisterVCAfterPop(controller: self)
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
--- VisitorBook/Controllers/RegisterViewController.swift
+++ VisitorBook/Controllers/RegisterViewController.swift
@@ -6,24 +6,55 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    var delegate: BackSettingVCProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
-    */
+    
+    @IBAction func toSettingVC(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func toLoginVC(_ sender: Any) {
+        self.delegate?.toLoginVCAfterPop(controller: self)
+    }
+    
+    @IBAction func registerUser(_ sender: Any) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.registerUserWithAuthAndFirestore(email: email, password: password) { success in
+            if success {
+                print("新規登録に成功しました")
+            } else {
+                print("新規登録に失敗しました")
+            }
+        }
+    }
+}
 
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
