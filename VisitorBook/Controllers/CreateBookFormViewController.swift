8c313bcc1af910c38ba7d7cfc937b56c4d1b61dd
matsui kento <matsui_2000@matsuinoMacBook-Pro.local>
2021-10-24 13:05:29 +0900

    Create CreateBookFormViewController

---
+++ VisitorBook/Controllers/CreateBookFormViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateBookFormViewController.swift
+//  VisitorBook
+//
+//  Created by matsui kento on 2021/10/24.
+//
+
+import UIKit
+
+class CreateBookFormViewController: UIViewController {
+
+    @IBOutlet weak var deceasedNameTextField: UITextField!
+    @IBOutlet weak var chiefMournerNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func createDone(_ sender: Any) {
+    }
+    
+}
--- VisitorBook/Controllers/CreateDescriptionViewController.swift
+++ VisitorBook/Controllers/CreateDescriptionViewController.swift
@@ -16,6 +16,10 @@ class CreateDescriptionViewController: UIViewController {
     }
     
     @IBAction func toCreateBookFormVC(_ sender: Any) {
+        guard let createBookFormVC = self.storyboard?.instantiateViewController(withIdentifier: "CreateBookFormVC") as? CreateBookFormViewController else {
+            fatalError("PasswordViewController is not found")
+        }
+        self.navigationController?.pushViewController(createBookFormVC, animated: true)
     }
     
     
